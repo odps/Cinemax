390b84727e2531d3c74ba53fb4885de6d04c3f01
odps <odps31@gmail.com>
2025-05-11 15:56:17 +0200

    [Frontend]: Implemented reservation of entradas funcionality on the detalles of peliculas

--- App/frontend/src/app/core/components/movie-card/movie-card.component.ts
+++ App/frontend/src/app/core/components/movie-card/movie-card.component.ts
@@ -1,9 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
 import { DialogModule } from 'primeng/dialog';
 import { Pelicula } from '../../interfaces/pelicula';
 import { assetsLocation } from '../../../../environments/environment';
+import { Funcion } from '../../interfaces/funcion';
+import { Sala } from '../../interfaces/sala';
 
 @Component({
   selector: 'app-movie-card',
@@ -15,11 +17,36 @@ import { assetsLocation } from '../../../../environments/environment';
 export class MovieCardComponent {
   @Input() pelicula!: Pelicula;
   @Input() isHighlighted: boolean = false;
+  @Output() reservarEntradas = new EventEmitter<Pelicula>();
   protected readonly assetsLocation = assetsLocation;
 
   mostrarModal: boolean = false;
+  mostrarSalasModal: boolean = false;
 
   mostrarDetalles() {
     this.mostrarModal = true;
+  }
+
+  abrirSalasModal() {
+    this.mostrarSalasModal = true;
+  }
+
+  cerrarSalasModal() {
+    this.mostrarSalasModal = false;
+  }
+
+  agruparFuncionesPorSala(funciones: Funcion[]) {
+    const grupos: { sala: any; funciones: Funcion[] }[] = [];
+    const mapa = new Map<number, { sala: any; funciones: Funcion[] }>();
+    for (const funcion of funciones) {
+      const salaId = funcion.idSala?.id;
+      if (!salaId) continue;
+      if (!mapa.has(salaId)) {
+        mapa.set(salaId, { sala: funcion.idSala, funciones: [] });
+        grupos.push(mapa.get(salaId)!);
+      }
+      mapa.get(salaId)!.funciones.push(funcion);
+    }
+    return grupos;
   }
 }
